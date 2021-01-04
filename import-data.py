324f1a2048ac6157d8ebf37d0e743b4d5992d70d
RePierre <petru.rebeja@gmail.com>
2021-01-04 16:53:17 +0200

    Added patter for files to process

--- import-data.py
+++ import-data.py
@@ -1,9 +1,26 @@
 import logging
 from argparse import ArgumentParser
+from zipfile import ZipFile
+import re
 
 
-def run(input_file):
-    pass
+def import_data(input_file, include_files=None):
+    """Reads the contents of the input archive and prepares the files for import.
+
+    Parameters
+    ----------
+    input_file: str, required
+        The full path to the zip archive containing files to import.
+    include_files: str, optional
+        A RegEx pattern that in matched against the full path of each
+        file within archive. If a value is provided then only files with
+        matching file names will be processed.
+        Default is None which means process all files.
+    """
+    with ZipFile(input_file) as zip_archive:
+        for f in zip_archive.namelist():
+            if (not include_files) or (re.match(include_files, f)):
+                print(f)
 
 
 def parse_arguments():
@@ -11,6 +28,8 @@ def parse_arguments():
 
     parser.add_argument('--input-file',
                         help="Full path of the input archive (zip) file.")
+    parser.add_argument('--include-files',
+                        help="The regex pattern for files to include")
     return parser.parse_args()
 
 
@@ -18,5 +37,5 @@ if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
                         level=logging.INFO)
     args = parse_arguments()
-    run(**args.__dict__)
+    import_data(**args.__dict__)
     logging.info("That's all folks!")
