075f52f08fbdeadca3655e1c32f2e710d06da3b1
RePierre <petru.rebeja@gmail.com>
2021-10-05 16:42:21 +0300

    Added export steps to the run-export script

--- run-export.sh
+++ run-export.sh
@@ -1,3 +1,5 @@
 source .venv/bin/activate;
-echo "Hello World!";
+python export-annotations.py --db-server $1 --db-name $2 --user $3 --password $4;
 deactivate;
+zip -r annotations.zip export;
+mv -f annotations.zip /var/export/
