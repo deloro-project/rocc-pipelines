eb10a987a7360c4dff0e134c1a8965dac25cd6f6
RePierre <petru.rebeja@gmail.com>
2021-10-21 16:45:21 +0300

    Updated export script with download of spacy corpus

--- export-lexicon.sh
+++ export-lexicon.sh
@@ -1,4 +1,6 @@
 source .venv/bin/activate;
+pip install -r requirements.txt
+python -m spacy download ro_core_news_lg
 python export-lexicon.py --db-server $1 --db-name $2 --user $3 --password $4 --output-dir ./lexii
 deactivate;
 zip -r lexii.zip lexii;
