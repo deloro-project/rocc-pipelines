847fd4fa6261fd95b5d64e3cd0065121cb3633f5
RePierre <petru.rebeja@gmail.com>
2021-10-21 17:15:52 +0300

    Removed pip install instruction from export script

--- export-lexicon.sh
+++ export-lexicon.sh
@@ -1,5 +1,4 @@
 source .venv/bin/activate;
-pip install -r requirements.txt
 python -m spacy download ro_core_news_lg
 python export-lexicon.py --db-server $1 --db-name $2 --user $3 --password $4 --output-dir ./lexii
 deactivate;
