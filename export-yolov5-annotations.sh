1e3b8555a6087080f5d83184445cab9bc213959e
RePierre <petru.rebeja@gmail.com>
2022-05-10 21:49:24 +0300

    Added default value for image size in export script

--- export-yolov5-annotations.sh
+++ export-yolov5-annotations.sh
@@ -1,6 +1,12 @@
+DB_SERVER=$1
+DB_NAME=$2
+USER=$3
+PASSWORD=$4
+IMG_SIZE=${5:-2048}
+
 rm -rf yolo-export
 source .venv/bin/activate;
-python export-yolov5-annotations.py --db-server $1 --db-name $2 --user $3 --password $4 --image-size $5 $6;
+python export-yolov5-annotations.py --db-server $DB_SERVER --db-name $DB_NAME --user $USER --password $PASSWORD --image-size $IMG_SIZE $IMG_SIZE;
 deactivate;
 zip -r yolov5-annotations.zip yolo-export/letters;
 mv -f yolov5-annotations.zip /var/export/
