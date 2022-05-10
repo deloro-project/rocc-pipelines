2cec485737cfb50d9773b8f0bf5131bfcda81d72
RePierre <petru.rebeja@gmail.com>
2022-05-10 15:50:25 +0300

    Duplicate value of imageSize input parameter when passing to script

--- export-yolov5-annotations.sh
+++ export-yolov5-annotations.sh
@@ -1,6 +1,6 @@
 rm -rf yolo-export
 source .venv/bin/activate;
-python export-yolov5-annotations.py --db-server $1 --db-name $2 --user $3 --password $4 --image-size $5 $5;
+python export-yolov5-annotations.py --db-server $1 --db-name $2 --user $3 --password $4 --image-size $5 $6;
 deactivate;
 zip -r yolov5-annotations.zip yolo-export/letters;
 mv -f yolov5-annotations.zip /var/export/
