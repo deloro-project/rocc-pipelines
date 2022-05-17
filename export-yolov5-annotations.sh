5161bd1dc97c2f8a1a57681a961d9dd3bcd4a782
RePierre <petru.rebeja@gmail.com>
2022-05-17 13:30:09 +0300

    Added argument for top labels to shell script

--- export-yolov5-annotations.sh
+++ export-yolov5-annotations.sh
@@ -3,10 +3,11 @@ DB_NAME=$2
 USER=$3
 PASSWORD=$4
 IMG_SIZE=${5:-2048}
+TOP_LABELS=${6:-1}
 
 rm -rf yolo-export
 source .venv/bin/activate;
-python export-yolov5-annotations.py --db-server $DB_SERVER --db-name $DB_NAME --user $USER --password $PASSWORD --image-size $IMG_SIZE $IMG_SIZE;
+python export-yolov5-annotations.py --db-server $DB_SERVER --db-name $DB_NAME --user $USER --password $PASSWORD --image-size $IMG_SIZE $IMG_SIZE --top-labels $TOP_LABELS;
 deactivate;
 zip -r yolov5-annotations.zip yolo-export/letters;
 mv -f yolov5-annotations.zip /var/export/
