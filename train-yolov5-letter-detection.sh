8b6dbb3e1c6233231831b7a2bce1f94bbc53e51c
RePierre <petru.rebeja@gmail.com>
2022-07-07 09:39:51 +0300

    Updated training script to run on remote machines

--- train-yolov5-letter-detection.sh
+++ train-yolov5-letter-detection.sh
@@ -1,35 +1,68 @@
 #!/bin/bash
 
-YOLO_DIR=${1:-~/yolov5/}
-ARCHIVE_PATH=${2:-/var/export/yolov5-annotations.zip}
-IMG_SIZE=${3:-1024}
-EPOCHS=${4:-1}
-MODEL=${5:-yolov5s.pt}
+# Script arguments
+SERVER=$1
+USER=$2
+PASSWORD=$3
+MODEL=$4
+
+# Constants
+YOLO_DIR=/var/training/yolov5
+EPOCHS=(300 600 900)
 
 
+# Create directory where to store training data.
+mkdir -p /var/training/data
+
+# Create directory where to store experiment results.
+mkdir -p /var/training/experiments
+
+# Navigate to data directory.
+cd /var/training/data
+
+# Remove old data if present.
+rm -rf *.zip
+
+# Secure copy the training data.
+sshpass -p $PASSWORD scp $USER@$SERVER:/var/export/yolov5-annotations-*.zip
+
+# Navigate to the yolov5 directory
 cd $YOLO_DIR
-cd data
 
-# Remove old data if present
-rm -rf yolov5-annotations.zip yolo-export deloro
+# Iterate training params and train for each combination.
+for num_epochs in "${EPOCHS[@]}" do
+    for file in /var/training/data/*.zip; do
+        # Remove old training data if present.
+        rm -rf yolo-export deloro
+        # Extract training data.
+        unzip $file -d .
+        # Rename yolo-export to deloro.
+        mv yolo-export deloro
+        # Fix paths in YAML file
+        sed -i 's/yolo-export\//data\/deloro\//g' deloro/characters/characters.yaml
+        # Get the resolution from file name by splitting the file name by '-' and taking the 3rd field.
+        img_size=$(echo $file | cut -d'-' -f3)
+        # Get the image type. First, split by '.' to remove the extension, and then split by '-' and take the 4th field.
+        img_type=$(echo $file | cut -d'.' -f1 | cut -d'-' -f4)
+        # Activate virtual environment.
+        cd $YOLO_DIR
+        source .venv/bin/activate
+        # Start training
+        python train.py --img $img_size --batch-size -1 --epochs $num_epochs --data data/deloro/characters/characters.yaml --device cpu --weights $MODEL
+        # Deactivate virtual environment.
+        deactivate
 
-# Copy and extract training data
-cp $ARCHIVE_PATH .
-unzip yolov5-annotations.zip -d .
-
-# Rename yolo-export to deloro
-mv yolo-export deloro
-
-# Fix paths in YAML files
-sed -i 's/yolo-export\//data\/deloro\//g' deloro/letters/letters.yaml
-sed -i 's/yolo-export\//data\/deloro\//g' deloro/characters/characters.yaml
-
-# Activate virtual environment
-cd $YOLO_DIR
-source .venv/bin/activate
-
-# Start training
-python train.py --img $IMG_SIZE --batch-size -1 --epochs $EPOCHS --data data/deloro/characters/characters.yaml --device cpu --weights $MODEL
-
-# Deactivate virtual environment
-deactivate
+	# Export run results to /var/training/experiments.
+	# Navigate to results directory.
+	cd $YOLO_DIR/runs/train
+	# Get model name without extension.
+	$model_name=$(echo $MODEL | cut -d'.' -f1)
+	# Build the file name of the archive of current results.
+	$experiment_archive="${model_name}-epochs-${num_epochs}-size-${img_size}-type-${img_type}.zip"
+	# Archive the run results.
+	zip -r $experiment_archive exp/
+	mv -f $experiment_archive /var/training/experiments
+	# Remove the run results
+	rm -rf exp
+    done
+done
